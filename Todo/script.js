319110a4fbe85b182d6edc9fa15895b0116e8b88
gauthampkrishnan <gauthampkrishnan@ieee.org>
2022-09-03 09:46:01 -0400

    JavaScript based Todo Project

---
+++ Todo/script.js
@@ -0,0 +1,39 @@
+//Selectors
+
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listener
+todoButton.addEventListener('click',addTodo);
+
+
+//Function
+
+function addTodo(event){
+    //prevent form from submittng
+    event.preventDefault();
+    //Todo div
+    const todoSDiv =document.createElement("div");
+    todoSDiv.classList.add("todo");
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoSDiv.appendChild(newTodo);
+    //Checked mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML='<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoSDiv.appendChild(completedButton);
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML='<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoSDiv.appendChild(trashButton);
+ 
+    //Append to list
+    todoList.appendChild(todoSDiv);
+    //clear todo input value
+    todoInput.value="";
+}
+
